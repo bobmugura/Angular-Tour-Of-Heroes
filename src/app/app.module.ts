7909add666022ad8b710b00ab91e0264a31bbb0a
Jasper Lee <bobmugura@gmail.com>
2018-08-18 23:58:19 -0400

    added Dashboard with routing between components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,21 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-  ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
+    FormsModule,
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
